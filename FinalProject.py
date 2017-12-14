cacc9a4678651193df4948ba3f205bdcffa07924
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-14 14:35:42 -0500

    Updated from Brython Server: 12/14/2017 2:35:41 PM

--- FinalProject.py
+++ FinalProject.py
@@ -2,3 +2,13 @@
 #12/14/17
 #FinalProject.py - OTHELLO
 
+from ggame import *
+
+if __name__ == '__main__':
+    
+    p1Color = Color(0x000000,1)
+    p2Color = Color(0xFFFFFF,1)
+    p1Circle = 
+    p2Circle = EllipseAsset(50,50,LineStyle(1,p2Color,),p2color)
+    Sprite(p2Circle)
+    App().Run()
