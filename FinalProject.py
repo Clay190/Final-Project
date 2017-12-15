3688124c98261f7d94e6eabef8ead91e653941af
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-15 14:49:06 -0500

    Updated from Brython Server: 12/15/2017 2:49:05 PM

--- FinalProject.py
+++ FinalProject.py
@@ -5,20 +5,31 @@
 from ggame import *
 
 def buildBoard():
-    data['board'] = [['']*8]*8
+    for i in range(0,8):
+        data['board'].append(['']*8)
     return data['board']
 
-
+'''
+def redrawAll():
+    for row in range(0,8):
+        for col in range(0,8):
+            for item in App().data['board'][:]:
+                item.destroy()
+            return data['board']
+            '''
+            
 if __name__ == '__main__':
     
     data = {}
-    data['board'] = []
+    data['board'] = ['d']
+    data['yeet'] = []
     
     p1Color = Color(0x00FF00,1)
     p2Color = Color(0xFF00FF,1)
     p2Circle = EllipseAsset(40,40,LineStyle(1,p2Color),p2Color)
     
     buildBoard()
+    #redrawAll()
     print(data['board'])
-    Sprite(p2Circle)
+    Sprite(p2Circle,(50,50))
     App().run()
