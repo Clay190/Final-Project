4568cbce93205fe954e53da3a43cce9bdbdda995
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-19 12:51:49 -0500

    Updated from Brython Server: 12/19/2017 12:51:48 PM

--- FinalProject.py
+++ FinalProject.py
@@ -4,12 +4,13 @@
 
 from ggame import *
 
-RADIUS = 20
+RADIUS = 30
 LINESIZE = 1.5
-P1COLOR = Color(0x00FF00,1)
-P2COLOR = Color(0xFF00FF,1)
+P1COLOR = Color(0x00F300,1)
+P2COLOR = Color(0x0080FF,1)
 WHITE = Color(0xFFFFFF,1)
 BLACK = Color(0x000000,1)
+
 P1CIRCLE = EllipseAsset(RADIUS,RADIUS,LineStyle(LINESIZE,BLACK),P1COLOR)
 P2CIRCLE = EllipseAsset(RADIUS,RADIUS,LineStyle(LINESIZE,BLACK),P2COLOR)
 WHITECIRCLE = EllipseAsset(RADIUS,RADIUS,LineStyle(LINESIZE,BLACK),WHITE)
@@ -17,7 +18,10 @@ WHITECIRCLE = EllipseAsset(RADIUS,RADIUS,LineStyle(LINESIZE,BLACK),WHITE)
 def buildBoard():
     for i in range(0,8):
         data['board'].append(['']*8)
-    data['board'][3][2] = 1
+    data['board'][3][3] = 2
+    data['board'][3][4] = 1
+    data['board'][4][3] = 1
+    data['board'][4][4] = 2
     return data['board']
 
 
@@ -29,16 +33,16 @@ def redrawAll():
             if data['board'][row][col] == '':
                 Sprite(WHITECIRCLE,(RADIUS+(col*(RADIUS*2)),RADIUS+(row*RADIUS*2)))
             elif data['board'][row][col] == 1:
-                Sprite(P1CIRCLE,([row]*RADIUS,[col]*RADIUS))
+                Sprite(P1CIRCLE,((row*RADIUS*2)+RADIUS,RADIUS+(col*RADIUS*2)))
             else:
-                Sprite(P2CIRCLE,((RADIUS*7),RADIUS*9))
+                Sprite(P2CIRCLE,((row*RADIUS*2)+RADIUS,RADIUS+(col*RADIUS*2)))
     
     
 if __name__ == '__main__':
     
     data = {}
     data['board'] = []
-    data['yeet'] = []
+    
     buildBoard()
     print(data['board'])
     redrawAll()
