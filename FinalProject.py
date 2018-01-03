02309a1dee4357a18b2fcbe66b48c3edf151e6e2
Clay190 <31509660+Clay190@users.noreply.github.com>
2018-01-03 13:29:22 -0500

    Updated from Brython Server: 1/3/2018 1:29:21 PM

--- FinalProject.py
+++ FinalProject.py
@@ -8,7 +8,7 @@ RADIUS = 30
 LINESIZE = 1.4
 P1COLOR = Color(0xFFFFFF,1)
 P2COLOR = Color(0x000000,1)
-BOARDCOLOR = Color(0x888888,0.5)
+BOARDCOLOR = Color(0x999999,0.5)
 BLACK = Color(0x000000,1)
 
 P1CIRCLE = EllipseAsset(RADIUS,RADIUS,LineStyle(LINESIZE,BLACK),P1COLOR)
@@ -27,6 +27,7 @@ def buildBoard():
 def boardFull():
     for i in range(0,8):
         if '' in data['board'][i]:
+            print('yea')
             return False
     else:
         return True
@@ -60,25 +61,92 @@ def redrawAll():
                 Sprite(P1CIRCLE,((row*RADIUS*2)+RADIUS,RADIUS+(col*RADIUS*2)))
             else:
                 Sprite(P2CIRCLE,((row*RADIUS*2)+RADIUS,RADIUS+(col*RADIUS*2)))
-                
+'''
 def flipNorth(x,y):
     if data['board'][x][y] == 1:
         data['board'][x][y-1] = 2
     elif data['board'][x][y] == 2:
         data['board'][x][y-1] = 1
     else:
-        print('didnt work')
+        print('flipNorth didnt work')
     redrawAll()
+
+def flipSouth(x,y):
+    if data['board'][x][y] == 1:
+        data['board'][x][y+1] = 2
+    elif data['board'][x][y] == 2:
+        data['board'][x][y+1] = 1
+    else:
+        print('flipSouth didnt work')
+    redrawAll()
+'''
+def flipEast(x,y):
+    i=1
+    while data['board'][x][y+i] == 1:
+        i+=1
+    if data['board'][x][y+i] != '':
+        data['board'][x][y+i] = 2
+        redrawAll()
+    
+'''    
+def flipWest(x,y):
+    if data['board'][x][y] == 1:
+        data['board'][x-1][y] = 2
+    elif data['board'][x][y] == 2:
+        data['board'][x-1][y] = 1
+    else:
+        print('flipWest didnt work')
+    redrawAll()
+
+def flipNorthEast(x,y):
+    if data['board'][x][y] == 1:
+        data['board'][x+1][y-1] = 2
+    elif data['board'][x][y] == 2:
+        data['board'][x+1][y-1] = 1
+    else:
+        print('flipNorthEast didnt work')
+    redrawAll()
+
+def flipNorthWest(x,y):
+    if data['board'][x][y] == 1:
+        data['board'][x-1][y-1] = 2
+    elif data['board'][x][y] == 2:
+        data['board'][x-1][y-1] = 1
+    else:
+        print('flipNorthWest didnt work')
+    redrawAll()
+    
+def flipSouthEast(x,y):
+    if data['board'][x][y] == 1:
+        data['board'][x+1][y+1] = 2
+    elif data['board'][x][y] == 2:
+        data['board'][x+1][y+1] = 1
+    else:
+        print('flipSouthEast didnt work')
+    redrawAll()
+
+def flipSouthWest(x,y):
+    if data['board'][x][y] == 1:
+        data['board'][x-1][y+1] = 2
+    elif data['board'][x][y] == 2:
+        data['board'][x-1][y+1] = 1
+    else:
+        print('flipSouthWest didnt work')
+    redrawAll()
+    '''
+def flipPieces(x,y):
+    print('Do flipPieces function')
     
 if __name__ == '__main__':
     
     data = {}
     data['board'] = []
-    data['row'] = 3
-    data['col'] = 3
+    data['row'] = 4
+    data['col'] = 4
     
     buildBoard()
-    flipNorth(data['col'],data['row'])
+    flipPieces(data['col'],data['row'])
+    flipEast(data['col'],data['row'])
     print(data['board'])
     redrawAll()
     boardFull()
